da15cc8ef738520cc552ce80a4fa6ddc167a9ad1
DanielKnafel <52681699+DanielKnafel@users.noreply.github.com>
2021-06-16 19:02:54 +0300

    Constrained joystick movement

--- app/src/main/java/com/example/remotecontroljoystick/view/JoystickView.kt
+++ app/src/main/java/com/example/remotecontroljoystick/view/JoystickView.kt
@@ -9,6 +9,8 @@ import android.view.View
 import androidx.annotation.RequiresApi
 import com.example.remotecontroljoystick.OnJoystickChange
 import java.lang.Integer.min
+import kotlin.math.pow
+import kotlin.math.sqrt
 
 class JoystickView @JvmOverloads constructor(
     context: Context,
@@ -16,25 +18,55 @@ class JoystickView @JvmOverloads constructor(
     defStyleAttr: Int = 0
     ) : View(context, attrs, defStyleAttr) {
     // Radius of the joystick
-    private var radius = 0.0f
-    private var centerX = 0.0f
-    private var centerY = 0.0f
+    private var outerCenterX = 0.0f
+    private var outerCenterY = 0.0f
+    private var innerCenterX = 0.0f
+    private var innerCenterY = 0.0f
+    private var innerRadius = 0.0f
+    private var outerRadius = 0.0f
     lateinit var onChange : OnJoystickChange
 
     init {
-
-        var listener = View.OnTouchListener(function = { view, motionEvent ->
+        val listener = OnTouchListener(function = { view, motionEvent ->
             view.performClick()
-            if (motionEvent.action == MotionEvent.ACTION_MOVE) {
-                centerX = motionEvent.x
-                centerY = motionEvent.y
+            if (motionEvent.action == MotionEvent.ACTION_MOVE) { // user drags
+                // calculate the distance between the new point and the original center
+                val distance = calculateDistance(motionEvent.x, motionEvent.y)
+                // if its a valid point within the circle
+                if (distance < outerRadius) {
+                    innerCenterX = motionEvent.x
+                    innerCenterY = motionEvent.y
+                }
+                else { // use the parallel triangles identity
+                    // calculate the maximal point of the inner circle in this direction
+                    val x =  motionEvent.x - outerCenterX
+                    val y =  motionEvent.y - outerCenterY
+                    // calculate the edge ratio
+                    val ratio = outerRadius / distance
+                    // calculate the edge length of the inner triangle
+                    val a = y * ratio
+                    val b = x * ratio
+                    innerCenterX = outerCenterX + b
+                    innerCenterY = outerCenterY + a
+                }
             }
+            else if (motionEvent.action == MotionEvent.ACTION_UP) { // put the inner circle back in the middle
+                innerCenterX = outerCenterX
+                innerCenterY = outerCenterY
+            }
+            // redraw the view
             invalidate()
             true
         })
         this.setOnTouchListener(listener)
     }
 
+    private fun calculateDistance(x : Float, y : Float) : Float {
+        // calculate the distance
+        val dx = (x - outerCenterX).pow(2)
+        val dy = (y - outerCenterY).pow(2)
+        return sqrt(dx + dy)
+    }
     private val outerPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         style = Paint.Style.FILL
         textAlign = Paint.Align.CENTER
@@ -49,24 +81,28 @@ class JoystickView @JvmOverloads constructor(
 
     @RequiresApi(Build.VERSION_CODES.N)
     override fun onSizeChanged(width: Int, height: Int, oldWidth: Int, oldHeight: Int) {
+        outerCenterX = (width / 2).toFloat()
+        outerCenterY = (height / 2).toFloat()
+        innerCenterX = outerCenterX
+        innerCenterY = outerCenterY
         // Calculate the radius from the smaller of the width and height.
-        centerX = (width / 2).toFloat()
-        centerY = (height / 2).toFloat()
-        radius = (min(width, height) / 2.0 * 0.8).toFloat()
+        outerRadius = (min(width, height) / 3.0 * 0.8).toFloat()
+        innerRadius = outerRadius / 2;
     }
 
-    private fun drawJoystick(canvas : Canvas, x : Float, y : Float) {
+    private fun drawJoystick(canvas : Canvas) {
         // draw outer circle
-        canvas.drawCircle(x, y, radius, outerPaint)
+        canvas.drawCircle(outerCenterX, outerCenterY, outerRadius, outerPaint)
         // draw inner circle
-        canvas.drawCircle(x, y, radius/2, innerPaint)
+        canvas.drawCircle(innerCenterX, innerCenterY, innerRadius, innerPaint)
     }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         // Draw the joystick
-        drawJoystick(canvas, centerX, centerY)
+        drawJoystick(canvas)
+        val ailerone =  (innerCenterX - outerCenterX) / outerRadius
+        val elivator = (innerCenterY - outerCenterY) / outerRadius
+        onChange.invoke(ailerone, elivator)
     }
-
-
 }
